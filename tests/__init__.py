3662f433d8e5af235f112b79e2c3864a4e45824c
Ashraf Emad <ashrafemad23@gmail.com>
2022-03-18 21:28:09 +0200

    add another test case for binary search

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,4 @@
 from .test_binary_search import test_binary_search_high_success
 from .test_binary_search import test_binary_search_low_success
 from .test_binary_search import test_binary_search_failed_to_find
+from .test_binary_search import test_binary_search_identical_list
--- tests/test_binary_search.py
+++ tests/test_binary_search.py
@@ -11,3 +11,7 @@ def test_binary_search_low_success():
 
 def test_binary_search_failed_to_find():
     assert binary_search([1, 2, 3, 4, 5], 6) is None, "Should be None"
+
+
+def test_binary_search_identical_list():
+    assert binary_search([0, 0, 0, 0], 0) == 1, "Should be 1 because the mid initial value will be 1"
